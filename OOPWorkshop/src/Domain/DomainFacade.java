6b0b12f5fe31c530db38427b611743393b3443f9
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2017-11-22 13:43:47 +0100

    Github spas

--- OOPWorkshop/src/Domain/DomainFacade.java
+++ OOPWorkshop/src/Domain/DomainFacade.java
@@ -5,6 +5,10 @@
  */
 package Domain;
 
+import Aqr.IBuilding;
+import Aqr.IDomainFacade;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,17 +16,41 @@ import java.util.Map;
  *
  * @author Victor Gram
  */
-public class DomainFacade {
-    public List<Building> buildingList;
+public class DomainFacade implements IDomainFacade {
+    public List<IBuilding> buildingList;
     public Map<Integer, Map<Integer, Map<Integer, Map<Integer, Map<Integer, Double>>>>> readingHistory;
     
-    
-    public Building getbuilding(int i) { 
-        return buildingList.get(i);
+    public DomainFacade () {
+        buildingList = new ArrayList<>(); 
+        readingHistory = new HashMap<>(); 
     }
     
-    public void addBuilding (Building building) { 
+    
+    public void addBuilding (IBuilding building) { 
         buildingList.add(building);
     }
+
+    @Override
+    public IBuilding getBuilding(int i) {
+        return buildingList.get(i);
+    }
+
+    @Override
+    public List<IBuilding> getBuildingList() {
+        return buildingList;
+    }
+
+    @Override
+    public void addBuilding(String name, String location) {
+        buildingList.add((IBuilding) new Building(name, location)); 
+    }
+
+    @Override
+    public void removeBuilding(int i) {
+        buildingList.remove(i); 
+    }
+    
+    hellohewifoh wrf
+            
     
 }
